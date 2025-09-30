4671f7dfacf49ee4201fdfb2b68ed1a31416b728
Oubaid-Beldi <oubaidbeldi@gmail.com>
2025-09-30 17:27:17 +0100

    msg

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  adapter: adapter({
-    excludeDatastoreFromEngineFunction: false
-  }),
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
